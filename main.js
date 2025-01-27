1dad6e35474b2c6be25cc1905c337c1400b5a0aa
espadv69 <aitorjob69@gmail.com>
2025-01-27 18:51:24 +0100

    Add DOM elements

---
+++ main.js
@@ -0,0 +1,9 @@
+// DOM elements
+const $lengthInput = document.querySelector('.length')
+const $includeLowercase = document.querySelector('.include-lowercase')
+const $includeUppercase = document.querySelector('.include-uppercase')
+const $includeNumbers = document.querySelector('.include-numbers')
+const $includeSymbols = document.querySelector('.include-symbols')
+const $generatePassButton = document.querySelector('.generate-password')
+const $passwordDisplay = document.querySelector('.password-display')
+const $copyButton = document.querySelector('.copy-password')
